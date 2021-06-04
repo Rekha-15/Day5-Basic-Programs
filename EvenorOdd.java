9fdca57864b729270fc02439c19054b82eb26db1
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-03 22:37:54 -0400

    add : add.

---
+++ EvenorOdd.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+import java.util.*;
+
+public class EvenorOdd 
+{
+	
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter The Number");
+			int num = sc.nextInt();
+			//Checking number is even or odd
+			if(num % 2 == 0)
+			{
+				System.out.println("Number is Even");
+			}
+			else
+			{
+				System.out.println("Number is Odd");
+			}
+		}
+	}
